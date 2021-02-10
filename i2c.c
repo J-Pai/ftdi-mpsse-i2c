dc8972cdeb32f09100b1526cbd8a71ce83038e3e
j-pai <jesse.pai@gmail.com>
2021-02-10 14:13:11 -0500

    Effectively slower clock.

--- i2c.c
+++ i2c.c
@@ -8,8 +8,8 @@ struct ftdi_context ftdic;
 unsigned char OutputBuffer[1024];
 // Buffer to hold Data unsigned chars to be read from FT4232H
 unsigned char InputBuffer[1024];
-// Value of clock divisor, SCL Frequency = 60MHz/(((1+0x00c8)*2) ~= 100KHz
-const uint32_t kClockDivisor = 0x00c8;
+// Value of clock divisor, SCL Frequency = 60MHz/(((1+0x012C)*2) ~= 100KHz
+const uint32_t kClockDivisor = 0x012C;
 // Index of output buffer
 uint16_t numberOfBytesToSend = 0;
 // Bytes sent
